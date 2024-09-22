437a40400e473869dafec33662e3ad0fe68a6be2
Nizar Fazari <nexsel32@gmail.com>
2024-09-22 16:49:01 +0700

    Choosing Bean to Depedency Ijenction

--- src/main/java/belajarspring/spring/core/DependecyInjectionConfiguration.java
+++ src/main/java/belajarspring/spring/core/DependecyInjectionConfiguration.java
@@ -3,14 +3,22 @@ package belajarspring.spring.core;
 import belajarspring.spring.core.bean.data.Bar;
 import belajarspring.spring.core.bean.data.Foo;
 import belajarspring.spring.core.bean.data.FooBar;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class DependecyInjectionConfiguration {
 
     @Bean
-    public Foo foo(){
+    @Primary
+    public Foo fooFirst(){
+        return  new Foo();
+    }
+
+    @Bean
+    public Foo fooSecond(){
         return  new Foo();
     }
 
@@ -21,7 +29,7 @@ public class DependecyInjectionConfiguration {
 
 
     @Bean
-    public FooBar fooBar(Foo foo, Bar bar){
+    public FooBar fooBar(@Qualifier("fooSecond") Foo foo, Bar bar){
         return new FooBar(foo,bar);
     }
 
--- src/test/java/belajarspring/spring/core/DepedencyIjectionTest.java
+++ src/test/java/belajarspring/spring/core/DepedencyIjectionTest.java
@@ -34,7 +34,7 @@ public class DepedencyIjectionTest {
     @Test
     void TestDI() {
 
-        Foo foo = applicationContext.getBean(Foo.class);
+        Foo foo = applicationContext.getBean( "fooSecond" , Foo.class);
         Bar bar = applicationContext.getBean(Bar.class);
         FooBar fooBar = applicationContext.getBean(FooBar.class);
 
